7b5e68e1884a6720b345d8ef9b58aabd8fed6507
pbucek <p_bucek@utb.cz>
2022-12-22 18:12:18 +0100

    added moshi

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,6 +17,8 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -24,15 +26,18 @@ import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
 private val retrofit = Retrofit.Builder()
-   .addConverterFactory(ScalarsConverterFactory.create())
-   .baseUrl(BASE_URL)
-   .build()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties():
-            Call<String>
+    suspend fun getProperties(): List<MarsProperty>
 }
 
 
